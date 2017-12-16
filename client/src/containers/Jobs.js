e72186707995754918ad0acbc2b9436605948681
chance-gao <chance-gao@hotmail.com>
2017-12-16 13:39:21 -0800

    change job page add button

--- client/src/containers/Jobs.js
+++ client/src/containers/Jobs.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 import {JobBox} from '../components/JobBox'
 
-
 class Jobs extends Component {
   render() {
+    const buttonStyle = {
+      'text-align': 'center'
+    }
+
     return (
       <div>
         <div className="row">
-          <div className="col-md-10">
+          <div className="col-sm-12" style={buttonStyle}>
+            <Button color="primary">Add Job</Button>
+          </div>  
+          <div className="col-sm-12">
             <div className="row row-eq-height">
               <JobBox />
               <JobBox />
@@ -17,10 +23,6 @@ class Jobs extends Component {
               <JobBox />
               <JobBox />
             </div>
-          </div>
-          <div className="col-md-2">
-            <p>HELLO THIS IS TEST</p>
-            <Button color="primary">Add Job</Button>
           </div>
         </div>
       </div>
