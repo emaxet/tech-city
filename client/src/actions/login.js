a39545420376765451d17167dc4e4b6b65b5d68e
Denny Park <denny.park93@gmail.com>
2017-12-18 15:50:20 -0800

    try to get login session

--- client/src/actions/login.js
+++ client/src/actions/login.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 export function login(payload) {
   return dispatch => {
-    return axios.post('http://localhost:3000/session/login', payload);
+    return axios.post('http://localhost:3000/session/login', payload).then(res => {
+      console.log(res);
+    });
   }
 }
