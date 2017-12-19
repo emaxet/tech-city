e82292a4f716a4f147543f115a6e3cd24d0dfe09
emaxet <etammpere@gmail.com>
2017-12-19 21:09:06 +0000

    removed localhost:3000 prefix from routes

--- client/src/actions/login.js
+++ client/src/actions/login.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function login(payload) {
   return dispatch => {
-    return axios.post('http://localhost:5000/session/login', payload).then(res => {
+    return axios.post('/session/login', payload).then(res => {
       console.log(res);
     });
   }
