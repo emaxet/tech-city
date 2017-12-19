85d58abaf79c06006f6c0795d953d0c4fd6b125e
Denny Park <denny.park93@gmail.com>
2017-12-19 14:08:41 -0800

    login error flash messages

--- client/src/actions/formValidations.js
+++ client/src/actions/formValidations.js
@@ -35,3 +35,26 @@ export function registerValidation(data) {
     }
   }
 }
+
+export function loginValidation(data) {
+  return dispatch => {
+    let errors = {};
+
+    if(!Validator.isEmail(data.email)) {
+      errors.email = 'Email is invalid';
+    }
+
+    if(Validator.isEmpty(data.email)) {
+      errors.email = 'Email is required';
+    }
+
+    if(Validator.isEmpty(data.password)) {
+      errors.password = 'Password is required';
+    }
+
+    return {
+      errors,
+      isValid: isEmpty(errors)
+    }
+  }
+}
