c46802d2c8c03b64a00655ed5565320a7275e8b7
Erik Tammpere <erik@Eriks-MacBook-Air.local>
2018-01-03 23:44:04 -0800

    When loading home page, server now sends back the total number of users, events and jobs in a city

--- server/routes/api/cities.js
+++ server/routes/api/cities.js
@@ -4,11 +4,30 @@ const cityHelpers = require('./lib/city-helpers');
 
 module.exports = (knex) => {
 
-  cities.get('/cities', (req, res) => {
-    cityHelpers.findAllCities(knex, (cities) => {
-      res.json(cities);
-    });
-  });
+  	cities.get('/cities', (req, res) => {
+
+	    cityHelpers.findAllCities(knex, (cities) => {
+	    	const cityData = [];
+
+	    	function sleep(period){
+  				return new Promise(resolve => setTimeout(() => resolve(), period));
+			}
+
+			async function run() {
+				for (let i = 0; i < cities.length; i++) {
+					await sleep(50);
+		    		cityHelpers.findCityData(knex, cities[i], (data) => {
+				    	cityData.push(data);
+				    	if (i === cities.length - 1) {
+				    		return res.json(cityData);
+				    	}
+		    		});
+	    		}
+			}
+
+			run();
+	    });
+  	});
 
   return cities;
 
